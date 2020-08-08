9be971f5660048914bf0164eaeddde0ddcdd6ebd
pablo <pablo@radius.net.br>
2020-08-07 23:35:07 -0300

    Tela inicial desenvolvida + inicio da inclusao de rotas

--- App.tsx
+++ App.tsx
@@ -1,17 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Landing from './src/pages/Landing';
+// import { StyleSheet, Text, View } from 'react-native';
+// import Landing from './src/pages/Landing';
+import { AppLoading } from 'expo';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+import Appstack from './src/routes/AppStack';
+
 
 export default function App() {
-  return (
-    //React native nao permite retorno de 2 elemento se nao estiverm dentro de algo
-    //Nome disso e fragment
-    <>
-      <Landing />
-      <StatusBar style="auto" />
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+  
+  if (!fontsLoaded){
+    return <AppLoading />
+  }else{
+    return (
+      //React native nao permite retorno de 2 elemento se nao estiverm dentro de algo
+      //Nome disso e fragment
+      <>
+        <Appstack />
+        <StatusBar style="light" />
+      </>
+    );
+  }
+  
 }
 
 
